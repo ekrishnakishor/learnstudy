5cc8de31f680168612156d7f554b1312a535678d
ekrishnakishor <ekrishnakishor@gmail.com>
2026-03-30 19:15:27 +0530

    host change

--- game_backend/config/settings.py
+++ game_backend/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-259mbz^q*g$xh%r2q5@67gx!54%8f-$^kgvbdu1py601$k7o)-
 DEBUG = True
 
 # ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = ['https://learnstudy-gzii.onrender.com/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['https://learnstudy-gzii.onrender.com/', 'learnstudy-gzii.onrender.com' 'localhost', '127.0.0.1', '*']
 
 
 # Application definition
